77f087a6954591e6ccc31a34c52eb0b1efca01d2
Rafael Ferreira Gomes <faelfg@gmail.com>
2025-03-17 18:16:13 -0300

    add unstaged files

---
+++ project/backend/bootstrap.php
@@ -0,0 +1,69 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use App\Core\Kernel;
+use Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+
+$apiPrefix = '/api';
+
+$apiRoutesFilepath = '/app/Routes/api.php';
+
+$webRoutesFilepath = '/app/Routes/web.php';
+
+Dotenv::createUnsafeImmutable(__DIR__)->load();
+
+$kernel = new Kernel();
+
+$container = $kernel->getContainer();
+
+//dd($container);
+
+// /Captura a requisição HTTP
+$request = Request::createFromGlobals();
+
+$context = new RequestContext();
+
+$context->fromRequest($request);
+
+$isRouteFileApi = str_contains($request->getPathInfo(), $apiPrefix);
+
+$routeFile = $isRouteFileApi ? __DIR__ . $apiRoutesFilepath : __DIR__ . $webRoutesFilepath;
+
+$routes = require $routeFile;
+
+$matcher = new UrlMatcher($routes, $context);
+
+try {
+    $method = '__invoke';
+
+    $parameters = $matcher->match($request->getPathInfo());
+
+    $controllerClass = $parameters['_controller'];
+
+    if (str_contains($controllerClass, '@')) {
+        [$controllerClass, $method] = explode('@', $controllerClass);
+    }
+
+    $controller = $container->get($controllerClass);
+
+    $response = call_user_func([$controller, $method]);
+
+    if ($isRouteFileApi && $response instanceof JsonResponse) {
+        $response = new JsonResponse(
+            ['data' => json_decode($response->getContent())],
+            $response->getStatusCode()
+        );
+    }
+} catch (HttpException $e) {
+    $response = errorExceptionResponse($isRouteFileApi, $e->getMessage(), $e->getStatusCode());
+} catch (Exception $e) {
+    $response = errorExceptionResponse($isRouteFileApi, $e->getMessage());
+}
+
+$response->send();
--- project/backend/public/index.php
+++ project/backend/public/index.php
@@ -1,11 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+require __DIR__ . '/../bootstrap.php';
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Backend</title>
 </head>
 <body>
-    <h1>Backend UP!!!</h1>
+    <h1>BACKEND UP!!!!</h1>
 </body>
 </html>
